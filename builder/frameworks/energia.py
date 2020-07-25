545a3352b40d31350d4220e6e31969202565383d
nanosonde <2073569+nanosonde@users.noreply.github.com>
2020-07-25 15:19:29 +0200

    Update

---
+++ builder/frameworks/energia.py
@@ -0,0 +1,102 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Energia
+
+Energia Wiring-based framework enables pretty much anyone to start easily
+creating microcontroller-based projects and applications. Its easy-to-use
+libraries and functions provide developers of all experience levels to start
+blinking LEDs, buzzing buzzers and sensing sensors more quickly than ever
+before.
+
+http://energia.nu/reference/
+"""
+
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-energiaticc13x0")
+FRAMEWORK_VERSION = platform.get_package_version("framework-energiaticc13x0")
+assert isdir(FRAMEWORK_DIR)
+
+
+env.Append(
+    CPPDEFINES=[
+        ("ARDUINO", 10805),
+        ("ENERGIA", int(FRAMEWORK_VERSION.split(".")[1])),
+        ("printf", "iprintf")
+    ],
+
+    CCFLAGS=[
+        "-mfloat-abi=hard",
+        "-mfpu=fpv4-sp-d16",
+        "-mabi=aapcs",
+        "--param", "max-inline-insns-single=500"
+    ],
+
+    LINKFLAGS=[
+        "-Wl,--entry=ResetISR",
+        # "-Wl,--cref",
+        "-Wl,--check-sections",
+        "-Wl,--gc-sections",
+        "-Wl,--unresolved-symbols=report-all",
+        "-Wl,--warn-common",
+        "-Wl,--warn-section-align",
+        "-mfloat-abi=hard",
+        "-mfpu=fpv4-sp-d16",
+        "-fsingle-precision-constant"
+    ],
+
+    LIBS=["libdriverlib"],
+
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "system"),
+        join(FRAMEWORK_DIR, "system", "inc"),
+        join(FRAMEWORK_DIR, "system", "driverlib"),
+        join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core")),
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+    ],
+
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "variants",
+             env.BoardConfig().get("build.variant")),
+        join(FRAMEWORK_DIR, "system", "driverlib")
+    ],
+
+
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries")
+    ]
+)
+
+if not env.BoardConfig().get("build.ldscript", ""):
+    env.Replace(LDSCRIPT_PATH=env.BoardConfig().get("build.arduino.ldscript", ""))
+
+#
+# Target: Build Core Library
+#
+
+libs = []
+
+libs.append(env.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkEnergia"),
+    join(FRAMEWORK_DIR, "cores", env.BoardConfig().get("build.core"))
+))
+
+env.Append(LIBS=libs)
---
+++ builder/main.py
@@ -0,0 +1,189 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Builder for Texas Instruments
+    Tiva C Series ARM Cortex-M4 microcontrollers.
+"""
+
+import sys
+from os.path import join
+
+from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Builder, Default, DefaultEnvironment)
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board = env.BoardConfig()
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    GDB="arm-none-eabi-gdb",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGSUFFIX=".elf"
+)
+
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-Os",  # optimize for size
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-Wall",
+        "-mthumb",
+        "-nostdlib"
+    ],
+
+    CFLAGS=[
+        "-std=gnu11"
+    ],
+
+    CXXFLAGS=[
+        "-fno-rtti",
+        "-fno-exceptions",
+        "-fno-threadsafe-statics",
+        "-std=gnu++11"
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-mthumb",
+        "-nostartfiles",
+        "-nostdlib"
+    ],
+
+    LIBS=["c", "gcc", "m"],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+if "BOARD" in env:
+    env.Append(
+        CCFLAGS=[
+            "-mcpu=%s" % board.get("build.cpu")
+        ],
+        LINKFLAGS=[
+            "-mcpu=%s" % board.get("build.cpu")
+        ]
+    )
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
+
+if "energia" in env.get("PIOFRAMEWORK", []):
+    sys.stderr.write(
+        "WARNING!!! Using of `framework = energia` in `platformio.ini` is "
+        "deprecated. Please replace with `framework = arduino`.\n")
+    env.Replace(PIOFRAMEWORK=["arduino"])
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+openocd_args = [
+    "-d%d" % (2 if int(ARGUMENTS.get("PIOVERBOSE", 0)) else 1)
+]
+openocd_args.extend(board.get("debug.tools.ti-icdi.server.arguments", []))
+openocd_args.extend([
+    "-c", "program {$SOURCE} %s verify reset; shutdown;" %
+    board.get("upload.offset_address", "")
+])
+openocd_args = [
+    f.replace("$PACKAGE_DIR",
+              platform.get_package_dir("tool-openocd") or "")
+    for f in openocd_args
+]
+env.Replace(
+    UPLOADER="openocd",
+    UPLOADERFLAGS=openocd_args,
+    UPLOADCMD="$UPLOADER $UPLOADERFLAGS")
+
+target_upload = env.Alias("upload", target_firm,
+                          env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE"))
+AlwaysBuild(target_upload)
+
+#
+# Target: Default targets
+#
+
+Default([target_buildprog, target_size])
